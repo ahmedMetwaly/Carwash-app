ea79436b872a5b7cf15d975aab9fac82c386078e
mokh <mahmouddado555@outlook.com>
2024-09-12 20:55:02 +0300

    finish edit name

--- lib/features/home/presentation/widgets/profile_details_item.dart
+++ lib/features/home/presentation/widgets/profile_details_item.dart
@@ -1,6 +1,10 @@
 import 'package:carwashapp/core/utils/values.dart';
 import 'package:carwashapp/features/auth/controller/auth_bloc/auth_bloc.dart';
+import 'package:carwashapp/features/auth/controller/user_bloc/user_bloc.dart';
+import 'package:carwashapp/features/auth/controller/user_bloc/user_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'change_profile_pic.dart';
 
 class ProfileDetails extends StatelessWidget {
@@ -16,21 +20,47 @@ class ProfileDetails extends StatelessWidget {
         const SizedBox(width: SizeManager.sSpace16),
         Expanded(
           flex: 3,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                AuthenticationBloc.user.name!,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
+          child: BlocBuilder<UserBloc, UserState>(
+            builder: (context, state) {
+              if (state is UpdatindDataState) {
+                return Skeletonizer(
+                  enabled: true,
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    AuthenticationBloc.user.name!,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const Text(
+                    'Personal Use',
+                    style: TextStyle(color: Colors.black45),
+                  ),
+                ],
               ),
-              const Text(
-                'Personal Use',
-                style: TextStyle(color: Colors.black45),
-              ),
-            ],
+                );
+              } else {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AuthenticationBloc.user.name!,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const Text(
+                      'Personal Use',
+                      style: TextStyle(color: Colors.black45),
+                    ),
+                  ],
+                );
+              }
+            },
           ),
         ),
       ],
