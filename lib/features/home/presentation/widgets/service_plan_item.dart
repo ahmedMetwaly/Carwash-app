6e6c92c2b05c177a400a64f4a08b5bedfb951c7b
mokh <mahmouddado555@outlook.com>
2024-09-14 20:24:20 +0300

    fix services screen

--- lib/features/home/presentation/widgets/service_plan_item.dart
+++ lib/features/home/presentation/widgets/service_plan_item.dart
@@ -20,123 +20,126 @@ class ServicePlanItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color primaryColor = Theme.of(context).colorScheme.primary;
 
-    return Card(
-      shadowColor: primaryColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-        side: BorderSide(
-          color: isSelected ? primaryColor : Colors.transparent,
+    return SizedBox(
+      width: MediaQueryUtils.getScreenWidth(context),
+      child: Card(
+        shadowColor: primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(
+            color: isSelected ? primaryColor : Colors.transparent,
+          ),
         ),
-      ),
-      margin: const EdgeInsets.all(8),
-      child: Padding(
-        padding: EdgeInsets.all(
-          MediaQueryUtils.getHeightPercentage(context, 0.011),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(
-                      img,
-                      height:
-                          MediaQueryUtils.getHeightPercentage(context, 0.060),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    Text(
-                      servicePlan.title,
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.check_circle,
-                          color: primaryColor,
-                        ),
-                        Text(
-                          servicePlan.services[0],
-                          style: const TextStyle(fontSize: 13),
-                        ),
-                        SizedBox(
-                          width: MediaQueryUtils.getWidthPercentage(
-                              context, 0.009),
-                        ),
-                        Icon(
-                          Icons.check_circle,
-                          color: primaryColor,
-                        ),
-                        Text(
-                          servicePlan.services[1],
-                          style: const TextStyle(fontSize: 13),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height:
-                          MediaQueryUtils.getWidthPercentage(context, 0.005),
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.check_circle,
-                          color: primaryColor,
-                        ),
-                        Text(
-                          servicePlan.services[2],
-                          style: const TextStyle(fontSize: 13),
-                        ),
-                        SizedBox(
-                          width: MediaQueryUtils.getWidthPercentage(
-                              context, 0.009),
-                        ),
-                        Icon(
-                          Icons.check_circle,
-                          color: primaryColor,
-                        ),
-                        Text(
-                          servicePlan.services[3],
-                          style: const TextStyle(fontSize: 13),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  "$price EGP",
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    color: ColorsManager.primary,
+        margin: const EdgeInsets.all(8),
+        child: Padding(
+          padding: EdgeInsets.all(
+            MediaQueryUtils.getHeightPercentage(context, 0.011),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        img,
+                        height:
+                            MediaQueryUtils.getHeightPercentage(context, 0.060),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Text(
+                        servicePlan.title,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                ),
-                Icon(
-                  isSelected
-                      ? Icons.radio_button_checked
-                      : Icons.radio_button_off,
-                  color: primaryColor,
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.check_circle,
+                            color: primaryColor,
+                          ),
+                          Text(
+                            servicePlan.services[0],
+                            style: const TextStyle(fontSize: 13),
+                          ),
+                          SizedBox(
+                            width: MediaQueryUtils.getWidthPercentage(
+                                context, 0.009),
+                          ),
+                          Icon(
+                            Icons.check_circle,
+                            color: primaryColor,
+                          ),
+                          Text(
+                            servicePlan.services[1],
+                            style: const TextStyle(fontSize: 13),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height:
+                            MediaQueryUtils.getWidthPercentage(context, 0.005),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.check_circle,
+                            color: primaryColor,
+                          ),
+                          Text(
+                            servicePlan.services[2],
+                            style: const TextStyle(fontSize: 13),
+                          ),
+                          SizedBox(
+                            width: MediaQueryUtils.getWidthPercentage(
+                                context, 0.009),
+                          ),
+                          Icon(
+                            Icons.check_circle,
+                            color: primaryColor,
+                          ),
+                          Text(
+                            servicePlan.services[3],
+                            style: const TextStyle(fontSize: 13),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    "$price EGP",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: ColorsManager.primary,
+                    ),
+                  ),
+                  Icon(
+                    isSelected
+                        ? Icons.radio_button_checked
+                        : Icons.radio_button_off,
+                    color: primaryColor,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
